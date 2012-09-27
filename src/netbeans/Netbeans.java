6b3920ea63a374900100ed5e045d2ac1664fb82f
Ricardo Pereira <tookietookie.88@gmail.com>
2012-09-27 19:08:18 +0100

    TESTInG!

--- src/netbeans/Netbeans.java
+++ src/netbeans/Netbeans.java
@@ -15,5 +15,6 @@ public class Netbeans {
      */
     public static void main(String[] args) {
         System.out.println("TESTE DO NETBEANS!");
+        System.out.println("TESTING AGAIN");
     }
 }
